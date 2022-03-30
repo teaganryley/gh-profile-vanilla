98cd86852fd2a7fe04a9734ae9cd8c8e75d033b5
tea <teaganschultz@gmail.com>
2022-03-30 17:22:47 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './styles';
+import theme from './theme';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Main from './routes/main';
+import Profile from './routes/profile';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Main />} />
+          <Route path="profile/:userId" element={<Profile />} />
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>There's nothing here!</p>
+              </main>
+            }
+          />
+        </Route>
+      </Routes>
+    </ThemeProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/theme.js
@@ -0,0 +1,10 @@
+export const theme = {
+  colors: {
+    text: '#3B4252',
+    background: '#FAFAFA',
+    primary: '#47525E',
+    secondary: '#3B4252',
+    accent: '#ECEFF4',
+    muted: '#8190A5',
+  },
+};

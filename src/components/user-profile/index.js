6d0ea9d4b2fbe0aa65686c6999d595865c56a7b8
tea <teaganschultz@gmail.com>
2022-04-07 17:18:31 -0400

    eslint config

--- src/components/user-profile/index.js
+++ src/components/user-profile/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 // import styled from 'styled-components';
 import { Row, Col } from 'react-grid-system';
+// style={{ height: '300px' }}
 
 const UserProfile = () => (
-  <Row align="center" justify="center" direction="row" style={{ height: '300px' }} debug>
+  <Row align="center" justify="center" direction="row" debug>
     <Col xs={3} debug>1 of 3</Col>
     <Col xs={3} debug>2 of 3</Col>
     <Col xs={3} debug>3 of 3</Col>

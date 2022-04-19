a02a413e7e0c92acd918080767afc84cbe932a4e
tea <teaganschultz@gmail.com>
2022-04-18 21:50:05 -0400

    static menu complete

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -3,26 +3,33 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { Row, Col } from 'react-grid-system';
 import StyledIcon from 'components/icon';
+import Button from 'components/button';
 import Typography from 'components/typography';
 import {
   follower,
   heart,
-  // bird,
-  // link,
-  // location,
-  // mail,
-  // org,
+  bird,
+  link,
+  location,
+  mail,
+  org,
   star,
 } from 'assets/icons';
 
-// re-style columns with custom component
 // styled-icon gets color props from theme provider?
 // stack component?
+// what happens when we render col without content?
 
 const Avatar = styled.img`
   display: block;
   width: 100%;
   margin: auto
+`;
+
+const StyledCol = styled(Col)`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
 `;
 
 const Menu = ({ user }) => (
@@ -54,36 +61,71 @@ const Menu = ({ user }) => (
     </Col>
     <Col xs={12}>
       <Row align="center" justify="between" direction="row" nogutter>
-        <Col
-          xs={4}
-          style={{
-            display: 'flex',
-            justifyContent: 'flex-start',
-            alignItems: 'center',
-          }}
-        >
+        <StyledCol xs={4}>
           <StyledIcon src={follower} color="#ECEFF4" />
           <Typography color="accent" size="s" fontStyle="italic">
             {user.followers}
             {' '}
             followers
           </Typography>
-        </Col>
-        <Col xs={4}>
+        </StyledCol>
+        <StyledCol xs={4}>
+          <StyledIcon src={heart} color="#ECEFF4" />
           <Typography color="accent" size="s" fontStyle="italic">
-            <StyledIcon src={heart} color="#ECEFF4" />
             {user.following}
             {' '}
             following
           </Typography>
-        </Col>
-        <Col xs={4}>
+        </StyledCol>
+        <StyledCol xs={4}>
+          <StyledIcon src={star} color="#ECEFF4" />
           <Typography color="accent" size="s" fontStyle="italic">
-            <StyledIcon src={star} color="#ECEFF4" />
             69 stars
           </Typography>
-        </Col>
+        </StyledCol>
       </Row>
+    </Col>
+    <StyledCol xs={12}>
+      <StyledIcon src={org} color="#ECEFF4" />
+      <Typography color="accent" fontStyle="italic">
+        organization
+      </Typography>
+    </StyledCol>
+    <StyledCol xs={12}>
+      <StyledIcon src={location} color="#ECEFF4" />
+      <Typography color="accent" fontStyle="italic">
+        location
+      </Typography>
+    </StyledCol>
+    <StyledCol xs={12}>
+      <StyledIcon src={mail} color="#ECEFF4" />
+      <Typography color="accent" fontStyle="italic">
+        email
+      </Typography>
+    </StyledCol>
+    <StyledCol xs={12}>
+      <StyledIcon src={link} color="#ECEFF4" />
+      <Typography color="accent" fontStyle="italic">
+        website
+      </Typography>
+    </StyledCol>
+    <StyledCol xs={12}>
+      <StyledIcon src={bird} color="#ECEFF4" />
+      <Typography color="accent" fontStyle="italic">
+        twitter
+      </Typography>
+    </StyledCol>
+    <Col
+      xs={12}
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        margin: 'auto',
+      }}
+    >
+      <Button variant="secondary">
+        Voltar
+      </Button>
     </Col>
   </Row>
 );
